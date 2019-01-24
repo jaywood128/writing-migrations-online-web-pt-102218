0dac12a40bab27ecc3c9ff9b898190aa4302da5c
Johnathon Wood <jaywood128@gmail.com>
2019-01-24 17:57:25 +0000

    Automatically backed up by Learn

---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,11 @@
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration 
+
+  def change 
+    add_column :students do |t| 
+      t.integer :grade 
+      t.text :birthdate
+    end
+  end
+
+
+end
